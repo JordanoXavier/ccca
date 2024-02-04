4cb94d16a5d8fa118d24b99536e8283bd5c3fb7a
JordanoXavier <112017191+JordanoXavier@users.noreply.github.com>
2024-02-04 11:15:56 -0300

    clean empty lines

--- backend/ride/src/main.ts
+++ backend/ride/src/main.ts
@@ -11,30 +11,21 @@ export function validateCpf (str: string) {
 					.replace('.','')
 					.replace('-','')
 					.replace(" ","");  
-	
 				if (!str.split("").every(c => c === str[0])) {
 					try{  
-						let     d1, d2;  
-						let     dg1, dg2, rest;  
-						let     digito;  
-							let     nDigResult;  
+						let d1, d2;  
+						let dg1, dg2, rest;  
+						let digito;  
+						let nDigResult;  
 						d1 = d2 = 0;  
 						dg1 = dg2 = rest = 0;  
 							
 						for (let nCount = 1; nCount < str.length -1; nCount++) {  
-							// if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-							// 	return false;
-							// } else {
-	
 								digito = parseInt(str.substring(nCount -1, nCount));  							
 								d1 = d1 + ( 11 - nCount ) * digito;  
-				
 								d2 = d2 + ( 12 - nCount ) * digito;  
-							// }
 						};  
-							
 						rest = (d1 % 11);  
-				
 						dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
 						d2 += 2 * dg1;  
 						rest = (d2 % 11);  
@@ -43,22 +34,17 @@ export function validateCpf (str: string) {
 						else  
 							dg2 = 11 - rest;  
 				
-							let nDigVerific = str.substring(str.length-2, str.length);  
+						let nDigVerific = str.substring(str.length-2, str.length);  
 						nDigResult = "" + dg1 + "" + dg2;  
+
 						return nDigVerific == nDigResult;
-						
-					// just in case...
 					}catch (e){  
 						console.error("Erro !"+e);  
-	
 						return false;  
 					}  
 				} else return false
-	
 			}else return false;
 		}
-
-
 	} else return false;
 }
 
@@ -66,33 +52,26 @@ export async function signup (input: any): Promise<any> {
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
 	try {
 		const id = crypto.randomUUID();
-
 		const [acc] = await connection.query("select * from cccat14.account where email = $1", [input.email]);
 		if (!acc) {
-
 			if (input.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
 				if (input.email.match(/^(.+)@(.+)$/)) {
-
 					if (validateCpf(input.cpf)) {
 						if (input.isDriver) {
 							if (input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
 								await connection.query("insert into cccat14.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
-								
-								const obj = {
+								return {
 									accountId: id
 								};
-								return obj;
 							} else {
 								// invalid car plate
 								return -5;
 							}
 						} else {
 							await connection.query("insert into cccat14.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
-
-							const obj = {
+							return {
 								accountId: id
 							};
-							return obj;
 						}
 					} else {
 						// invalid cpf
@@ -102,17 +81,14 @@ export async function signup (input: any): Promise<any> {
 					// invalid email
 					return -2;
 				}
-
 			} else {
 				// invalid name
 				return -3;
 			}
-
 		} else {
 			// already exists
 			return -4;
 		}
-
 	} finally {
 		await connection.$pool.end();
 	}
