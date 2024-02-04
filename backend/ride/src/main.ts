83b3d722711780805bd8ad9c2eeded27f6bbcb46
JordanoXavier <112017191+JordanoXavier@users.noreply.github.com>
2024-02-04 12:09:12 -0300

    create functions for regex validations in signup

--- backend/ride/src/main.ts
+++ backend/ride/src/main.ts
@@ -53,18 +53,30 @@ export async function signup (input: any): Promise<any> {
 	try {
 		const id = crypto.randomUUID();
 		const [existentAccount] = await connection.query("select * from cccat14.account where email = $1", [input.email]);
-		if(existentAccount) throw new Error("already exists");
-		if (!input.name.match(/[a-zA-Z] [a-zA-Z]+/)) throw new Error("invalid name");
-		if (!input.email.match(/^(.+)@(.+)$/)) throw new Error("invalid email");
+		if (existentAccount) throw new Error("already exists");
+		if (!validateName(input.name)) throw new Error("invalid name");
+		if (!validateEmail(input.email)) throw new Error("invalid email");
 		if (!validateCpf(input.cpf)) throw new Error("invalid cpf");
-		if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) throw new Error("invalid car plate");
+		if (input.isDriver && !validateCarPlate(input.carPlate)) throw new Error("invalid car plate");
 
 		await connection.query("insert into cccat14.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
-		
+
 		return { accountId: id };
 	} finally {
 		await connection.$pool.end();
 	}
+}
+
+function validateName (str: string) {
+	return str.match(/[a-zA-Z] [a-zA-Z]+/);
+}
+
+function validateEmail (str: string) {
+	return str.match(/^(.+)@(.+)$/);
+}
+
+function validateCarPlate (str: string) {
+	return str.match(/[A-Z]{3}[0-9]{4}/);
 }
 
 export async function getAccount (accountId: string) {
