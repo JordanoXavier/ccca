d3969d74a3f1bde6431b0b27c1985fe1cc304557
JordanoXavier <112017191+JordanoXavier@users.noreply.github.com>
2024-02-04 17:26:23 -0300

    add test to check if account_id is a passenger

--- backend/ride/test/ride.test.ts
+++ backend/ride/test/ride.test.ts
@@ -1,9 +1,6 @@
 import { signup } from "../src/account";
 import { getRide, requestRide } from "../src/ride";
 
-// * deve verificar se o account_id tem is_passenger true
-// * deve verificar se já não existe uma corrida do passageiro em status diferente de "completed", se existir lançar um erro
-
 test("Deve criar uma corrida", async function () {
     const passengerInput = {
 		name: "John Doe",
@@ -27,3 +24,25 @@ test("Deve criar uma corrida", async function () {
     expect(getRideOutput.driver).toBe(null);
     expect(getRideOutput.status).toBe("requested");
 });
+
+test("Não deve criar uma corrida se o account_id não for de um passageiro", async function () {
+    const passengerInput = {
+        name: "John Doe",
+        email: `john.doe${Math.random()}@gmail.com`,
+        cpf: "88946105003",
+        isPassenger: false,
+        isDriver: true,
+        carPlate: "ABC1234",
+        password: "admin123"
+    };
+    const passengerOutput = await signup(passengerInput);
+
+    const rideInput = {
+        passenger_id: passengerOutput.accountId,
+        position: { lat: 0, long: 0 },
+        destination: { lat: 10, long: 10 }
+    };
+    await expect(() => requestRide(rideInput)).rejects.toThrow(new Error("account is not a passenger"));
+});
+
+// * deve verificar se já não existe uma corrida do passageiro em status diferente de "completed"
