72c5db76c0a1db6bd5f7216f5fa282e1a334e2af
JordanoXavier <112017191+JordanoXavier@users.noreply.github.com>
2024-02-13 20:14:13 -0300

    refactor controllers

--- backend/ride/src/app.ts
+++ backend/ride/src/app.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express';
-import accountRouter from './router/accountRouter';
-import rideRouter from './router/rideRouter';
+import account from './controllers/account';
+import ride from './controllers/ride';
 
 const app = express();
 app.use(express.json());
  
-app.use("/account", accountRouter);
-app.use("/ride", rideRouter);
+app.use("/account", account);
+app.use("/ride", ride);
  
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     res.status(500).send(error.message);
