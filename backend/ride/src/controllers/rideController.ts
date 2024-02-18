f5bda3f431df07bc7229a1b6a6dff6b4807e58d9
JordanoXavier <112017191+JordanoXavier@users.noreply.github.com>
2024-02-18 18:24:49 -0300

    add controllers to accept and start ride

--- backend/ride/src/controllers/rideController.ts
+++ backend/ride/src/controllers/rideController.ts
@@ -3,6 +3,8 @@ import RideRepositoryDatabase from '../repositories/ride/RideRepositoryDatabase'
 import RequestRide from '../useCases/ride/RequestRide';
 import AccountRepositoryDatabase from '../repositories/account/AccountRepositotyDatabase';
 import GetRide from '../useCases/ride/GetRide';
+import AcceptRide from '../useCases/ride/AcceptRide';
+import StartRide from '../useCases/ride/StartRide';
 
 const router = express.Router();
 
@@ -20,6 +22,23 @@ router.get('/:id', async (req: Request, res: Response) => {
     const getRide = new GetRide(rideRepository);
 
     const ride = await getRide.execute(req.params.id);
+    res.send(ride);
+});
+
+router.post('/accept', async (req: Request, res: Response) => {
+    const rideRepository = new RideRepositoryDatabase();
+    const accountRepository = new AccountRepositoryDatabase();
+    const acceptRide = new AcceptRide(rideRepository, accountRepository);
+
+    const ride = await acceptRide.execute(req.body);
+    res.send(ride);
+});
+
+router.post('/start', async (req: Request, res: Response) => {
+    const rideRepository = new RideRepositoryDatabase();
+    const startRide = new StartRide(rideRepository);
+
+    const ride = await startRide.execute(req.body);
     res.send(ride);
 });
 
