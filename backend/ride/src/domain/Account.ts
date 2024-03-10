0e4f8d0cdad1635fcf5bf5f6be6aef75f48f51c3
JordanoXavier <112017191+JordanoXavier@users.noreply.github.com>
2024-03-10 19:18:19 -0300

    add cpf vo in domain

--- backend/ride/src/domain/Account.ts
+++ backend/ride/src/domain/Account.ts
@@ -2,23 +2,23 @@ import crypto from "crypto";
 import { validateCpf } from "./validateCpf";
 import CarPlate from "./CarPlate";
 import Name from "./Name";
+import Cpf from "./Cpf";
 
 export default class Account {
 	accountId: string;
 	name: Name;
 	email: string;
-	cpf: string;
+	cpf: Cpf;
 	carPlate: CarPlate | null;
 	isPassenger: boolean;
 	isDriver: boolean;
 
 	constructor (name: string, email: string, cpf: string, carPlate: string, isPassenger: boolean, isDriver: boolean, accountId?: string) {
 		if (!this.validateEmail(email)) throw new Error("invalid email");
-		if (!validateCpf(cpf)) throw new Error("invalid cpf");
 		this.accountId = accountId || crypto.randomUUID();
 		this.name = new Name(name);
 		this.email = email;
-		this.cpf = cpf;
+		this.cpf = new Cpf(cpf);
 		this.carPlate = isDriver ? new CarPlate(carPlate) : null;
 		this.isPassenger = isPassenger;
 		this.isDriver = isDriver;
