39941c5b8713fdf970fc5818c697f5a83095a49a
JordanoXavier <112017191+JordanoXavier@users.noreply.github.com>
2024-03-21 00:44:01 -0300

    add position domain

---
+++ backend/ride/src/domain/positionAggregate/Position.ts
@@ -0,0 +1,23 @@
+export default class Position {
+    positionId: string;
+    rideId: string;
+    lat: number;
+    long: number;
+    date: Date;
+
+    constructor (rideId: string, lat: number, long: number, date?: Date, positionId?: string, ) {
+        this.rideId = rideId;
+        this.lat = lat;
+        this.long = long;
+        if(positionId){
+            this.positionId = positionId;
+        } else {
+            this.positionId = crypto.randomUUID();
+        }
+        if(date){
+            this.date = date;
+        } else {
+            this.date = new Date();
+        }
+    }    
+}
