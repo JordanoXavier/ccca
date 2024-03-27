488a36bdd3fef23f8c89f017070fbb068fbfd6be
JordanoXavier <112017191+JordanoXavier@users.noreply.github.com>
2024-03-26 23:25:19 -0300

    refactor account tests

--- backend/ride/test/account.test.ts
+++ backend/ride/test/account.test.ts
@@ -11,21 +11,27 @@ beforeEach(async () => {
 	signup = new Signup(accountRepository);
 });
 
+function getInputExample() {
+	return {
+		name: "John Doe",
+		email: `john.doe${Math.random()}@gmail.com`,
+		cpf: "55335504021",
+		isPassenger: true,
+		password: "admin123",
+	};
+}
+
 test.each([
 	"88946105003",
 	"55335504021",
 	"58170101000"
 ])("Deve criar conta de passageiro", async function (cpf: string) {
 	const inputSignup = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
+		...getInputExample(),
 		cpf,
-		isPassenger: true,
-		password: "admin123"
 	};
 
 	const outputSignup = await signup.execute(inputSignup);
-	
 	const outputGetAccount = await getAccount.execute(outputSignup.accountId);
 
 	assert(outputGetAccount);
@@ -36,14 +42,10 @@ test.each([
 
 test("Deve criar a conta de motorista", async function () {
 	const inputSignup = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
-		cpf: "55335504021",
+		...getInputExample(),
 		isDriver: true,
-		password: "admin123",
 		carPlate: "ABC1234",
 	};
-
 	const outputSignup = await signup.execute(inputSignup);
 	const outputGetAccount = await getAccount.execute(outputSignup.accountId);
 
@@ -55,92 +57,55 @@ test("Deve criar a conta de motorista", async function () {
 
 test("Não deve criar a conta de motorista com placa de carro inválida", async function () {
 	const inputSignup = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
-		cpf: "55335504021",
+		...getInputExample(),
 		isDriver: true,
-		password: "admin123",
 		carPlate: "AB11234",
 	};
-
 	await expect(() => signup.execute(inputSignup)).rejects.toThrow(new Error("invalid car plate"));
 });
 
 test("Não deve criar conta com CPF inválido", async function () {
 	const inputSignup = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
+		...getInputExample(),
 		cpf: "aaaaaa",
-		isPassenger: true,
-		password: "admin123",
-		carPlate: "ABC1234",
 	};
-
 	await expect(() => signup.execute(inputSignup)).rejects.toThrow(new Error("invalid cpf"));
 });
 
 test("Não deve criar conta sem CPF", async function () {
 	const inputSignup = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
+		...getInputExample(),
 		cpf: "",
-		isPassenger: true,
-		password: "admin123",
-		carPlate: "ABC1234",
 	};
-
 	await expect(() => signup.execute(inputSignup)).rejects.toThrow(new Error("invalid cpf"));
 });
 
 test("Não deve criar conta com CPF com todos dígitos iguais", async function () {
 	const inputSignup = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
+		...getInputExample(),
 		cpf: "11111111111",
-		isPassenger: true,
-		password: "admin123",
-		carPlate: "ABC1234",
 	};
-
 	await expect(() => signup.execute(inputSignup)).rejects.toThrow(new Error("invalid cpf"));
 });
 
 test("Não deve criar conta com e-mail inválido", async function () {
 	const inputSignup = {
-		name: "John Doe",
+		...getInputExample(),
 		email: `john.doe${Math.random()}gmail.com`,
-		cpf: "55335504021",
-		isPassenger: true,
-		password: "admin123",
-		carPlate: "ABC1234",
 	};
-
 	await expect(() => signup.execute(inputSignup)).rejects.toThrow(new Error("invalid email"));
 });
 
 test("Não deve criar conta com nome inválido", async function () {
 	const inputSignup = {
+		...getInputExample(),
 		name: "John",
-		email: `john.doe${Math.random()}@gmail.com`,
-		cpf: "55335504021",
-		isPassenger: true,
-		password: "admin123",
-		carPlate: "ABC1234",
 	};
-
 	await expect(() => signup.execute(inputSignup)).rejects.toThrow(new Error("invalid name"));
 });
 
 test("Não deve criar conta com e-mail já existente", async function () {
-	const inputSignup = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
-		cpf: "55335504021",
-		isPassenger: true,
-		password: "admin123",
-		carPlate: "ABC1234",
-	};
-
+	const inputSignup = getInputExample();
 	await signup.execute(inputSignup);
 	await expect(() => signup.execute(inputSignup)).rejects.toThrow(new Error("already exists"));
 });
